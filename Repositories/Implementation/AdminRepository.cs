a7860425fb76583f5e47720cd769c59761589e9b
KUSH-TRAMBADIYA <kushtrambadiya@gmail.com>
2026-03-05 15:37:12 +0530

    Repo created

--- Repositories/Implementation/AdminRepository.cs
+++ Repositories/Implementation/AdminRepository.cs
@@ -3,11 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Repositories.Interfaces;
+using Repositories.Models;
+using Npgsql;
+
 
 namespace Repositories.Implementation
 {
     public class AdminRepository : IAdminInterface
     {
-        
+        private readonly NpgsqlConnection _conn;
+        public AdminRepository(NpgsqlConnection connection)
+        {
+            _conn = connection;
+        }
+
+
+        public Task<int> AbsentEmpCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountDesigningHour()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountDevelopingHour()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountResearchHour()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> GetEmployeeCount()
+        {
+            try
+            {
+                var query = "SELECT COUNT(*) FROM t_employees";
+
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, _conn);
+
+                await _conn.CloseAsync();
+                await _conn.OpenAsync();
+
+                var result = await cmd.ExecuteScalarAsync();
+
+                return Convert.ToInt32(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return -1;
+            }
+            finally
+            {
+                await _conn.CloseAsync();
+            }
+        }
+
+        public Task<List<EmployeeModel>> ListEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> OnLeaveEmpCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> PresentEmpCount()
+        {
+            // var query = "SELECT COUNT(DISTINCT c_empid) FROM t_attendance WHERE c_attenddate = CURRENT_DATE";
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateEmpStatus()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repositories/Interfaces/IAdminInterface.cs
+++ Repositories/Interfaces/IAdminInterface.cs
@@ -2,11 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Repositories.Models;
 
 namespace Repositories.Interfaces
 {
     public interface IAdminInterface
     {
-        
+        Task<int> GetEmployeeCount();
+
+        Task<int> PresentEmpCount();
+
+        Task<int> AbsentEmpCount();
+
+        Task<int> OnLeaveEmpCount();
+
+        Task<int> CountDevelopingHour();
+        Task<int> CountDesigningHour();
+        Task<int> CountResearchHour();
+
+        Task<List<EmployeeModel>> ListEmployee();
+
+        Task<int> UpdateEmpStatus();
     }
 }

d01ef4e151cdb3be1bfe56227d01ee357329f792
Jil02877 <140379056+Jil02877@users.noreply.github.com>
2026-03-21 00:35:03 +0530

    UI is done for leanding page. go for testing

--- MVCAttendEase/Program.cs
+++ MVCAttendEase/Program.cs
@@ -108,7 +108,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 async Task IndexElasticData()
 {
